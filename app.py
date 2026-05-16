d24d41d57f9a42792d5329e60337135f5c911d26
aladdin4220434 <aladdingamal044@gmail.com>
2026-05-16 16:43:14 +0300

    Update app.py

--- app.py
+++ app.py
@@ -268,4 +268,4 @@ def handle_subscribe(task_id):
     join_room(task_id)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False, allow_unsafe_werkzeug=True)
